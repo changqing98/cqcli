91cb684ed3f8d626eb7d3af3cb86ae274eee06b9
changqingyeh <yecq1998@outlook.com>
2020-08-02 10:08:51 +0800

    Init project version

--- cqcli/__init__.py
+++ cqcli/__init__.py
@@ -2,7 +2,7 @@ import logging
 
 from . import config
 
-version = '1.1.3'
+version = '0.0.0'
 
 # 设置 logging 的配置
 level = logging._nameToLevel[config.LOG_LEVEL.upper()]
--- setup.py
+++ setup.py
@@ -2,16 +2,17 @@ import os
 
 from setuptools import setup, find_packages
 
+version = '0.0.0'
 # read requirement.txt
 here = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(here, 'requirements.txt')) as f:
     requirements = [_.strip() for _ in f.readlines() if not _.startswith('#')]
 
 setup(
-    name="cqcli",
-    author="changqing",
-    author_email="yecq1998@outlook.com",
-    version="1.0.1",
+    name='cqcli',
+    author='changqing',
+    author_email='yecq1998@outlook.com',
+    version=version,
     install_requires=requirements,
     packages=find_packages()
 )
